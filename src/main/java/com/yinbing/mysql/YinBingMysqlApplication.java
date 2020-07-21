325712601ad67d87d153a138833be8b6fa8ab5ac
jixiang.wang <jixiang.wang@openjawtech.com>
2020-07-21 14:04:18 +0800

    Initial commit

---
+++ src/main/java/com/yinbing/mysql/YinBingMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.yinbing.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinBingMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinBingMysqlApplication.class, args);
+	}
+
+}
